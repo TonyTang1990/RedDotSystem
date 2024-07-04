b783e2906304a1566dccace4efe28af4dbe1101e
TonyTang <huantang0529@gmail.com>
2024-07-04 14:10:54 +0800

    Fix: 1. 支持直接获取指定红点名结果信息的接口，方便上层逻辑直接访问结果刷新显示

--- RedDotSystem/Assets/Scripts/GameLogic/RedDotSystem/RedDotManager.cs
+++ RedDotSystem/Assets/Scripts/GameLogic/RedDotSystem/RedDotManager.cs
@@ -36,6 +36,13 @@ public class RedDotManager : SingletonTemplate<RedDotManager>
     private Dictionary<string, string> mResultChangedRedDotNameMap;
 
     /// <summary>
+    /// 已经计算过的红点运算单元和结果是否变化Map<红点运算单元，结果是否变化>
+    /// Note:
+    /// 优化单个红点运算单元重复计算的问题
+    /// </summary>
+    private Dictionary<RedDotUnit, bool> mCaculatedRedDotUnitResultChangeMap;
+
+    /// <summary>
     /// 标脏检测更新帧率
     /// </summary>
     private const int DIRTY_UPDATE_INTERVAL_FRAME = 10;
@@ -66,6 +73,7 @@ public class RedDotManager : SingletonTemplate<RedDotManager>
         InitRedDotUnitResultMap();
         mDirtyRedDotUnitMap = new Dictionary<RedDotUnit, RedDotUnit>();
         //mResultChangedRedDotUnitList = new List<RedDotUnit>();
+        mCaculatedRedDotUnitResultChangeMap = new Dictionary<RedDotUnit, bool>();
         mResultChangedRedDotNameMap = new Dictionary<string, string>();
     }
 
@@ -108,24 +116,38 @@ public class RedDotManager : SingletonTemplate<RedDotManager>
             Debug.LogError($"触发红点名:{redDotName}刷新回调失败!");
             return false;
         }
+        var redDotNameResult = GetRedDotNameResult(redDotName);
+        redDotInfo.TriggerUpdate(redDotNameResult.result, redDotNameResult.redDotType);
+        return true;
+    }
+
+    /// <summary>
+    /// 获取指定红点名的结果数据
+    /// </summary>
+    /// <param name="redDotName"></param>
+    /// <returns></returns>
+    public (int result, RedDotType redDotType) GetRedDotNameResult(string redDotName)
+    {
+        (int result, RedDotType redDotType) redDotNameResult = (0, RedDotType.NONE);
         var redDotUnitList = RedDotModel.Singleton.GetRedDotUnitsByName(redDotName);
-        if(redDotUnitList != null)
+        if (redDotUnitList != null)
         {
             var result = 0;
             var redDotType = RedDotType.NONE;
-            foreach(var redDotUnit in redDotUnitList)
+            foreach (var redDotUnit in redDotUnitList)
             {
                 var redDotUnitResult = GetRedDotUnitResult(redDotUnit);
-                if(redDotUnitResult > 0)
+                if (redDotUnitResult > 0)
                 {
                     var redDotType2 = RedDotModel.Singleton.GetRedDotUnitRedType(redDotUnit);
                     redDotType = redDotType | redDotType2;
                 }
                 result += redDotUnitResult;
             }
-            redDotInfo.TriggerUpdate(result, redDotType);
+            redDotNameResult.result = result;
+            redDotNameResult.redDotType = redDotType;
         }
-        return true;
+        return redDotNameResult;
     }
 
     /// <summary>
@@ -271,17 +293,39 @@ public class RedDotManager : SingletonTemplate<RedDotManager>
         //        mResultChangedRedDotUnitList.Add(dirtyRedDotUnit.Key);
         //    }
         //}
-        // 触发已绑定的关心改变红点运算单元的红点刷新
+        // 部分红点单元单纯是影响因素，不会决定最终红点数量
+        // 所以这里要把标脏的红点单元影响到的红点名所有红点单元都触发计算看是否有相关结论变化
+        // 有相关红点单元的结果变化的红点名才需要通知上层逻辑回调刷新
+        mCaculatedRedDotUnitResultChangeMap.Clear();
         mResultChangedRedDotNameMap.Clear();
         foreach(var dirtyRedDotUnit in mDirtyRedDotUnitMap)
         {
-            var redDotUnit = dirtyRedDotUnit.Key;
-            var redDotNameList = RedDotModel.Singleton.GetRedDotUnitNames(redDotUnit);
-            if(redDotNameList != null)
+            var realRedDotUnit = dirtyRedDotUnit.Key;
+            var redDotNameList = RedDotModel.Singleton.GetRedDotUnitNames(realRedDotUnit);
+            if(redDotNameList == null || redDotNameList.Count == 0)
             {
-                foreach (var redDotName in redDotNameList)
+                continue;
+            }
+            foreach (var redDotName in redDotNameList)
+            {
+                var redDotUnitList = RedDotModel.Singleton.GetRedDotUnitsByName(redDotName);
+                if(redDotUnitList == null || redDotUnitList.Count == 0)
                 {
-                    if (!mResultChangedRedDotNameMap.ContainsKey(redDotName))
+                    continue;
+                }
+                foreach(var redDotUnit in redDotUnitList)
+                {
+                    bool resultChange = false;
+                    if(!mCaculatedRedDotUnitResultChangeMap.TryGetValue(redDotUnit, out resultChange))
+                    {
+                        resultChange = DoRedDotUnitCaculate(redDotUnit);
+                        mCaculatedRedDotUnitResultChangeMap.Add(redDotUnit, resultChange);
+                    }
+                    if(!resultChange)
+                    {
+                        continue;
+                    }
+                    if(!mResultChangedRedDotNameMap.ContainsKey(redDotName))
                     {
                         mResultChangedRedDotNameMap.Add(redDotName, redDotName);
                     }
@@ -289,7 +333,6 @@ public class RedDotManager : SingletonTemplate<RedDotManager>
             }
         }
         mDirtyRedDotUnitMap.Clear();
-        //mResultChangedRedDotUnitList.Clear();
         // 通过每个红点所有影响的红点运算单元得出红点显示结论并通知更新
         foreach (var resultChangedRedDotName in mResultChangedRedDotNameMap)
         {
